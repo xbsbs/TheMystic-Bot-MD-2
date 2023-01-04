51cfa38e0a09a263f73346a404990933635a0800
ELGAZAR_ALWAZER❤️ <113715409+Jhsishdjjsk@users.noreply.github.com>
2023-01-04 10:22:37 +0200

    Update convertidor-toimg.js

--- plugins/convertidor-toimg.js
+++ plugins/convertidor-toimg.js
@@ -1,6 +1,6 @@
 import { webp2png } from '../lib/webp2mp4.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
-const notStickerMessage = 'رد علي* الملصق التي تريد تحويلها إلى صورة باستخدام الأمر ${usedPrefix + command}*`
+const notStickerMessage = `*رد علي الملصق الذي تريد تحويله لصوره ${usedPrefix + command}*`
 if (!m.quoted) throw notStickerMessage
 const q = m.quoted || m
 let mime = q.mediaType || ''
@@ -9,7 +9,7 @@ let media = await q.download()
 let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
 await conn.sendFile(m.chat, out, 'error.png', null, m)
 }
-handler.help = ['لصوره (reply)']
+handler.help = ['toimg (reply)']
 handler.tags = ['sticker']
-handler.command = ['toimg', 'لصوره', 'لصورة']
+handler.command = ['لصوره', 'لصورة', 'img']
 export default handler

95d77947ac093601282f90bbaccd528c9b7b32db
ELGAZAR_ALWAZER❤️ <113715409+Jhsishdjjsk@users.noreply.github.com>
2023-01-04 11:06:24 +0200

    Update fun-ttt.js

--- plugins/fun-ttt.js
+++ plugins/fun-ttt.js
@@ -1,11 +1,11 @@
 import TicTacToe from '../lib/tictactoe.js'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 conn.game = conn.game ? conn.game : {}
-if (Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) throw '*[❗] مازلت في مباراه مع شخص ما*'
-if (!text) throw `*[❗] مطلوب وضع اسم لغرفه الالعاب*\n\n*—◉ مثال*\n*◉ ${usedPrefix + command} elgazar alwazer*`
+if (Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) throw '*[❗] 𝙰𝚄𝙽 𝙴𝚂𝚃𝙰𝚂 𝙴𝙽 𝚄𝙽 𝙹𝚄𝙴𝙶𝙾 𝙲𝙾𝙽 𝙰𝙻𝙶𝚄𝙸𝙴𝙽*'
+if (!text) throw `*[❗] 𝚂𝙴 𝚁𝙴𝚀𝚄𝙸𝙴𝚁𝙴 𝙿𝙾𝙽𝙴𝚁 𝚄𝙽 𝙽𝙾𝙼𝙱𝚁𝙴 𝙰 𝙻𝙰 𝚂𝙰𝙻𝙰 𝙳𝙴 𝙹𝚄𝙴𝙶𝙾*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾*\n*◉ ${usedPrefix + command} nueva sala*`
 let room = Object.values(conn.game).find(room => room.state === 'WAITING' && (text ? room.name === text : true))
 if (room) {
-await m.reply('*[🕹️] ابدأ اللعبة وانضم لاعب إلى اللعبة*')
+await m.reply('*[🕹️] 𝙸𝙽𝙸𝙲𝙸𝙰 𝙴𝙻 𝙹𝚄𝙴𝙶𝙾, 𝚄𝙽 𝙹𝚄𝙶𝙰𝙳𝙾𝚁 𝚂𝙴 𝚄𝙽𝙸𝙾 𝙰 𝙻𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰*')
 room.o = m.chat
 room.game.playerO = m.sender
 room.state = 'PLAYING'
@@ -24,7 +24,7 @@ O: '⭕',
 9: '9️⃣',
 }[v]})
 let str = `
-🎮 غرفه جديده 🎮
+🎮 𝐓𝐑𝐄𝐒 𝐄𝐍 𝐑𝐀𝐘𝐀 🎮
 
 ❎ = @${room.game.playerX.split('@')[0]}
 ⭕ = @${room.game.playerO.split('@')[0]}
@@ -33,7 +33,7 @@ let str = `
         ${arr.slice(3, 6).join('')}
         ${arr.slice(6).join('')}
 
-دور @${room.game.currentTurn.split('@')[0]}
+𝚃𝚄𝚁𝙽𝙾 𝙳𝙴 @${room.game.currentTurn.split('@')[0]}
 `.trim()
 if (room.x !== room.o) await conn.sendMessage(room.x, { text: str, mentions: this.parseMention(str)}, { quoted: m })
 await conn.sendMessage(room.o, { text: str, mentions: conn.parseMention(str)}, { quoted: m })
@@ -46,8 +46,8 @@ game: new TicTacToe(m.sender, 'o'),
 state: 'WAITING' }
 if (text) room.name = text     
 let imgplay = `https://cope-cdnmed.agilecontent.com/resources/jpg/8/9/1590140413198.jpg`
-conn.sendButton(m.chat, `*🕹 غرفه جديده 🎮*\n\n*◉ في انتظار لاعب*\n*◉ لحذف اللعبه ومغادره الجيم استخدم الامر ${usedPrefix}delttt*`, wm, imgplay, [['الانضام الي الغرفه', `${usedPrefix + command} ${text}`]], m, { mentions: conn.parseMention(text) })
+conn.sendButton(m.chat, `*🕹 𝐓𝐑𝐄𝐒 𝐄𝐍 𝐑𝐀𝐘𝐀 🎮*\n\n*◉ 𝙴𝚂𝙿𝙴𝚁𝙰𝙽𝙳𝙾 𝙰𝙻 𝚂𝙴𝙶𝚄𝙽𝙳𝙾 𝙹𝚄𝙶𝙰𝙳𝙾𝚁*\n*◉ 𝙿𝙰𝚁𝙰 𝙱𝙾𝚁𝚁𝙰𝚁 𝙾 𝚂𝙰𝙻𝙸𝚁𝚂𝙴 𝙳𝙴 𝙻𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰 𝚄𝚂𝙴𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix}delttt*`, wm, imgplay, [['𝚄𝙽𝙸𝚁𝚂𝙴 𝙰 𝙻𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰', `${usedPrefix + command} ${text}`]], m, { mentions: conn.parseMention(text) })
 conn.game[room.id] = room
 }}
-handler.command = /^(اكس-او|ttc|ttt|xo)$/i
+handler.command = /^(tictactoe|ttc|ttt|xo)$/i
 export default handler

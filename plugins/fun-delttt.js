6f79164ec047584dc334b93ead446694f241b4c0
ELGAZAR_ALWAZER❤️ <113715409+Jhsishdjjsk@users.noreply.github.com>
2023-01-04 11:04:24 +0200

    Update fun-delttt.js

--- plugins/fun-delttt.js
+++ plugins/fun-delttt.js
@@ -1,9 +1,9 @@
 import MessageType from '@adiwajshing/baileys'
 let handler = async (m, { conn, usedPrefix, command }) => {
 let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
-if (room == undefined) return conn.sendButton(m.chat, '*[❗] انت لست في اي مباراه*', wm, null, [['بدأ غرفه جديده', `${usedPrefix}ttt partida nueva`]], m)
+if (room == undefined) return conn.sendButton(m.chat, '*[❗] 𝙽𝙾 𝙴𝚂𝚃𝙰𝚂 𝙴𝙽 𝙽𝙸𝙽𝙶𝚄𝙽𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰 𝙳𝙴 𝚃𝚁𝙴𝚂 𝙴𝙽 𝚁𝙰𝚈𝙰*', wm, null, [['𝙸𝙽𝙸𝙲𝙸𝙰𝚁 𝚂𝙰𝙻𝙰 𝙳𝙴 𝙹𝚄𝙴𝙶𝙾', `${usedPrefix}ttt partida nueva`]], m)
 delete conn.game[room.id]
-await m.reply('*[ ✔ ] تم حذف الغرفه بنجاح*')}
+await m.reply('*[ ✔ ] 𝚂𝙴 𝙴𝙻𝙸𝙼𝙸𝙽𝙾 𝙻𝙰 𝚂𝙰𝙻𝙰 𝙳𝙴 𝙹𝚄𝙴𝙶𝙾 𝙳𝙴 𝚃𝚁𝙴𝚂 𝙴𝙽 𝚁𝙰𝚈𝙰*')}
 handler.command = /^(delttt|deltt|delxo|deltictactoe)$/i
 handler.fail = null
 export default handler
